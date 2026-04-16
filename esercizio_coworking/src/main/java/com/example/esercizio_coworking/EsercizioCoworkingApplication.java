f579bc97b03d20e899770c241044ceac0bc666ff
LucaMB <lmbaratti@gmail.com>
2026-04-16 14:46:40 +0200

    progetto

---
+++ esercizio_coworking/src/main/java/com/example/esercizio_coworking/EsercizioCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.esercizio_coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioCoworkingApplication.class, args);
+	}
+
+}
---
+++ esercizio_coworking/src/test/java/com/example/esercizio_coworking/EsercizioCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizio_coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
