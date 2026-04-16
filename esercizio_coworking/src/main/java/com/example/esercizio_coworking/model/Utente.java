173ef848e99bdfe8c9155c3f67042c97d6e1ab4a
Gabriele <gabriele.campomizzi@gmail.com>
2026-04-16 15:00:29 +0200

    utente.java

---
+++ esercizio_coworking/src/main/java/com/example/esercizio_coworking/model/Utente.java
@@ -0,0 +1,30 @@
+package com.example.esercizio_coworking.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Utente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String ruolo; // es: "USER", "MANAGER", "AMMINISTRATORE"
+
+    private String refreshToken;
+}
